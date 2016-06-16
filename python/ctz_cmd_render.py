7dede1f95a1f700bc024cb5eb6f4b8ea490f6677
cchetanpatel <cchetanpatel@gmail.com>
2016-06-16 22:26:34 +1200

    update to cmd render

--- python/ctz_cmd_render.py
+++ python/ctz_cmd_render.py
@@ -24,25 +24,62 @@ class CmdRender(QWidget):
             ctrl[0].setChecked(True)
             self.write_group.append(ctrl)
 
+        self.render_setting_layout = QHBoxLayout()
 
-        self.push = QPushButton("Genereate command")
-        self.push.setToolTip("Generates the command.")
-        self.push.clicked.connect(self.generate_cmd)
+        self.render_start_frame_label = QLabel("Start Frame: ")
+        self.render_end_frame_label = QLabel("End Frame: ")
+        self.render_start_frame_textedit = QLineEdit(str(int(nuke.root()["first_frame"].value())))
+        self.render_end_frame_textedit = QLineEdit(str(int(nuke.root()["last_frame"].value())))
+
+        self.render_setting_layout.addWidget(self.render_start_frame_label)
+        self.render_setting_layout.addWidget(self.render_start_frame_textedit)
+        self.render_setting_layout.addWidget(self.render_end_frame_label)
+        self.render_setting_layout.addWidget(self.render_end_frame_textedit)
+
+        self.copy_clipboard_button = QPushButton("Copy Cmd To Clipboard")
+        self.copy_clipboard_button.setToolTip("Copies the command to the clipboard.")
+        self.exe_clipboard_button = QPushButton("Copy Cmd To Clipboard")
+        self.exe_clipboard_button.setToolTip("Copies the command to the clipboard.")
+
+        self.copy_clipboard_button.clicked.connect(self.copy_clipboard)
+        self.exe_clipboard_button.clicked.connect(self.exe_cmd)
 
         self.label = QLabel("Path: ")
 
         self.layout = QVBoxLayout()
-        for w in self.write_group:
-            l = QHBoxLayout()
-            l.addSpacing(25)
-            for c in w:
-                l.addWidget(c)
-            self.layout.addLayout(l)
+        self.layout.addLayout(self.render_setting_layout)
 
+        tableWidget = QTableWidget()
+        tableWidget.setRowCount(len(self.writes))
+        tableWidget.setColumnCount(5)
+
+        count = 0;
+        for w in self.write_group:
+            tableWidget.setItem(count, 0, QTableWidgetItem(self.writes[count]["name"].value()))
+
+            chkBoxItem = QTableWidgetItem()
+            chkBoxItem.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+            chkBoxItem.setCheckState(Qt.Checked)
+            tableWidget.setItem(count, 1, chkBoxItem)
+
+
+            count += 1
+
+
+
+
+        # for w in self.write_group:
+        #     l = QHBoxLayout()
+        #     l.addSpacing(25)
+        #     for c in w:
+        #         l.addWidget(c)
+        #     self.layout.addLayout(l)
+        self.layout.addWidget(tableWidget)
         self.layout.addWidget(self.label)
 
         self.layout.addSpacing(25)
-        self.layout.addWidget(self.push)
+        self.layout.addWidget(self.copy_clipboard_button)
+        self.layout.addWidget(self.exe_clipboard_button)
 
         self.setLayout(self.layout)
 
@@ -57,9 +94,13 @@ class CmdRender(QWidget):
 
 
     def generate_cmd(self):
+        cmd_args = ""
+
+    def copy_clipboard(self):
         print "hello"
 
-
+    def exe_cmd(self):
+        print "hello"
 
 # start the panel
 def start():
